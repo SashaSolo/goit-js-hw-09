c71151d26e0350421fef9231b3702589f3636c15
Sasha.Solo <sasha.solomannik@gmail.com>
2022-10-05 00:01:15 +0300

    1 - task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const bodyElem = document.querySelector('body');
+
+const startBtn = document.querySelector('[data-start]');
+
+const stopBtn = document.querySelector('[data-stop]');
+
+startBtn.addEventListener('click', btnStart);
+
+stopBtn.addEventListener('click', btnStop);
+
+let timerId = null;
+
+function btnStart() {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+  timerId = setInterval(() => {
+    bodyElem.style.background = getRandomHexColor();
+  }, 1000);
+}
+
+function btnStop() {
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+  clearInterval(timerId);
+}
